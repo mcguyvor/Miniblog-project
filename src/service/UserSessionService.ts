15749b12352d2e651ff3530bdce8a832b0eba8aa
Nathakorn Throngtrairat <nathakorn.throngtrairat@agoda.com>
2019-12-05 01:32:46 +0700

    add return

--- src/service/UserSessionService.ts
+++ src/service/UserSessionService.ts
@@ -1,13 +1,14 @@
 import { auth } from 'firebase';
 import firebaseProvider from './FirebaseProvider';
 import AccountAPI from './API/AccountAPI';
+import User from '../model/User';
 
 export default class UserSessionService {
 
     private auth = auth(firebaseProvider.firebase)
     private accountAPI = new AccountAPI()
 
-    async register(email: string, displayName: string, password: string) {
+    async register(email: string, displayName: string, password: string): Promise<User> {
         const userCredential = await this.auth.createUserWithEmailAndPassword(email, password)
         if (userCredential.user) {
             const user = await this.accountAPI.register({
@@ -15,6 +16,7 @@ export default class UserSessionService {
                 displayName,
                 uid: userCredential.user.uid
             })
+            return user
         } else {
             throw Error("User is somehow null")
         }
