f7ea17e06535cdd57535fd6dcafac65c21b43441
Noppawat Chotiwan <noppawatchotiwan@gmail.com>
2019-12-05 01:10:23 +0700

    add login validate and onChange

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Main from './component/Main';
 import CreateBlog from './component/CreateBlog';
 import {Route,Switch} from 'react-router-dom';
+import LogInPage from './component/LogInPage';
 function App() {
   return (
     <Switch >
       <Route exact path = '/' component = {Main} />
       <Route exact path = '/createblog' component = {CreateBlog} />
-      <Route exact path = '/login' component = {CreateBlog} />
+      <Route exact path = '/login' component = {LogInPage} />
     </Switch>
   );
 }
--- src/component/LogInPage.js
+++ src/component/LogInPage.js
@@ -1,8 +1,74 @@
-import React from 'react';
+import React,{useState} from 'react';
+import '../style/Login.css';
 const LogInPage = () =>{
+
+    const initialInput = {email:''}
+
+    const [input,setInput] = useState(initialInput);
+
+    const initialValidate= {emailError :'', passwordError:''};
+
+    const [errorMessage,setError] =useState(initialValidate);
+
+    const validateForm = () =>{
+        let emailError ='';
+        const email =  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!input.email){
+            emailError ='Please enter your email address';
+        }
+        if(!input.email.match(email)){
+            emailError = 'Please enter the correct email format'; 
+        }
+        if(emailError){
+            setError({emailError:emailError});
+            return false;
+        }else{
+            return true;
+        }
+    }
+    const handleOnSubmit = (e) =>{
+        e.preventDefault();
+        const isValid = validateForm();
+        if(isValid){
+            // post method to end point
+            console.log(input);
+            setError(initialValidate);
+            e.target.reset();       
+        }
+    }
+
+    const handleChange  = (e) =>{
+        setInput({...input,[e.target.name]:e.target.value});
+    }
+
+
+    const renderLoginForm = () =>{
+        return(
+            
+                    <div className='container vertical-center'>
+                        <form className='w-100'>
+                            
+                            <div className="form-group">
+                                <label for="InputEmail"><strong>Email address</strong></label>
+                                <input type="email" onChange = {handleChange} className="form-control" id="InputEmail" aria-describedby="emailHelp" name='email' placeholder='Email address'/>
+                                {errorMessage.emailError ?<small id="emailHelp" className="form-text text-danger">{errorMessage.emailError}</small> : null}
+                            </div>
+                            
+                            <div className="form-group">
+                                <label for="InputPassword"><strong>Password</strong></label>
+                                <input type="password" onChange = {handleChange} className="form-control" id="InputPassword" name='password' placeholder='Password'/>
+                            </div>
+                            
+                            <button type="submit" className="btn btn-outline-success mr-2" onClick={handleOnSubmit}>Login</button>
+                            <button type="submit" className="btn btn-outline-primary">Sign In</button>
+                        </form>
+                    </div>
+                
+        );
+    }
     return(
         <div>
-            
+            {renderLoginForm()}
         </div>
     );
 }
