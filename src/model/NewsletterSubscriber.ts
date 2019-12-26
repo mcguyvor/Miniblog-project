62a5936d02a425b3bfec10f1d8420672a7579679
Nathakorn Throngtrairat <nathakorn.throngtrairat@agoda.com>
2019-12-26 15:47:29 +0700

    implement Newsletter service

---
+++ src/model/NewsletterSubscriber.ts
@@ -0,0 +1,4 @@
+export default interface NewsletterSubscriber {
+    email: string;
+    isCancelled: boolean;
+}
---
+++ src/service/API/NewsletterAPI.ts
@@ -0,0 +1,15 @@
+import BaseAPI from './BaseAPI'
+import NewsletterSubscriber from '../../model/NewsletterSubscriber'
+
+enum Path {
+    SUBSCRIPTION = '/subscription'
+}
+
+export default class NewsletterAPI extends BaseAPI {
+    private readonly BASE_PATH = '/newsletter'
+
+    async subscription(email: string): Promise<NewsletterSubscriber> {
+        const response = await this.post(this.BASE_PATH + Path.SUBSCRIPTION, { email })
+        return this.validateResponse(response)
+    }
+}
